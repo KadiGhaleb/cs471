3f53bab802cd40be26ee10d07afb34e3173c1227
KadiGhaleb <cadi.al7arbi@gmail.com>
2024-10-25 20:09:32 +0300

    Lecture 3 updates

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from apps.bookmodule import views
 
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('', views.index,name='index'), #the main root, http:127.0.0.1/8000/
+    path('books', views.books), #the root is books http:127.0.0.1/8000/books
+    path('book/<int:bId>', views.book)
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 def index(request):
     return render(request, 'bookmodule/index.html') #it will be in template folder
+
+def books(request):
+    u = request.user #the requesting user's info
+    #return redirect('/') #here is redirection to the root "it will go back to urls and to the index"
+    return render(request, 'bookmodule/booklist.html') #(request, template)
+
+def book(request, bId):
+    book1 = {'id':12344321, 'title': 'Continuous Delivery', 'author': 'by J. Humble and D. Farley'}
+    book2 = {'id':56788765, 'title': 'Reversing: Secrets of Reverse Engineering', 'author':'by E. Eilam'}
+
+    targetBook = None
+
+    if book1['id'] == bId: targetBook = book1
+    if book2['id'] == bId: targetBook = book2
+    
+    if targetBook == None:
+        return redirect('/books')
+    
+    context = {'book':targetBook} #book is the variable name accessible by template
+    return render(request, 'bookmodule/book.html', context)
